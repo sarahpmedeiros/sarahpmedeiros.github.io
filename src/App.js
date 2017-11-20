e501dbf4d49ed735400356bdc15196f9647cd29e
Sarah Medeiros <sarahpmedeiros@gmail.com>
2017-11-19 21:40:44 -0500

    fixed broken test and build

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/home/Home'
 import About from './pages/about/About'
 import {
     Route,
+    BrowserRouter as Router,
     Link
 } from 'react-router-dom'
 import {Menu} from 'semantic-ui-react'
@@ -14,22 +15,26 @@ class App extends Component {
     render() {
         return (
             <div>
-                <Header>
-                    <Menu style={{border:'none'}}secondary borderless attached='top'>
-                        <Menu.Menu position={'right'}>
-                            <Menu.Item as={Link} to={'/home'}>Home</Menu.Item>
-                            <Menu.Item as={Link} to={'/about'}>About</Menu.Item>
-                            <Menu.Item as={Link} to={'/resume'}>Resume</Menu.Item>
-                            <Menu.Item as={Link} to={'/contact'}>Contact Me</Menu.Item>
-                        </Menu.Menu>
-                    </Menu>
-                </Header>
-                <main>
-                    <Route exact path="/home" component={Home}/>
-                    <Route path="/about" component={About}/>
-                    <Route path="/resume" component={About}/>
-                    <Route path="/contact" component={About}/>
-                </main>
+                <Router>
+                    <div>
+                    <Header>
+                        <Menu style={{border: 'none'}} secondary borderless attached='top'>
+                            <Menu.Menu position={'right'}>
+                                <Menu.Item as={Link} to={'/home'}>Home</Menu.Item>
+                                <Menu.Item as={Link} to={'/about'}>About</Menu.Item>
+                                <Menu.Item as={Link} to={'/resume'}>Resume</Menu.Item>
+                                <Menu.Item as={Link} to={'/contact'}>Contact Me</Menu.Item>
+                            </Menu.Menu>
+                        </Menu>
+                    </Header>
+                    <main>
+                        <Route exact path="/home" component={Home}/>
+                        <Route path="/about" component={About}/>
+                        <Route path="/resume" component={About}/>
+                        <Route path="/contact" component={About}/>
+                    </main>
+                    </div>
+                </Router>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
-import {
-    BrowserRouter as Router,
-} from 'react-router-dom'
-
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
