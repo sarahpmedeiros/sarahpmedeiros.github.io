04d9f6a7984ef9a5e25782627ba7b5c567c5bfa6
Sarah Medeiros <sarahpmedeiros@gmail.com>
2017-11-23 22:47:45 -0500

    added about me

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
     HashRouter as Router,
     Link
 } from 'react-router-dom'
-import {Menu} from 'semantic-ui-react'
+import {Menu, Segment, Container, Icon, Sticky} from 'semantic-ui-react'
 
 class App extends Component {
 
@@ -20,21 +20,27 @@ class App extends Component {
                     <Header>
                         <Menu style={{border: 'none'}} secondary borderless attached='top'>
                             <Menu.Menu position={'right'}>
-                                <Menu.Item as={Link} to={'/home'}>Home</Menu.Item>
-                                <Menu.Item as={Link} to={'/about'}>About</Menu.Item>
-                                <Menu.Item as={Link} to={'/resume'}>Resume</Menu.Item>
-                                <Menu.Item as={Link} to={'/contact'}>Contact Me</Menu.Item>
+                                <Menu.Item as={Link} to={'home'}>Home</Menu.Item>
+                                <Menu.Item as={Link} to={'resume'}>Resume</Menu.Item>
+                                <Menu.Item as={Link} to={'about'}>About</Menu.Item>
                             </Menu.Menu>
                         </Menu>
                     </Header>
                     <main>
                         <Route exact path="/home" component={Home}/>
                         <Route path="/about" component={About}/>
-                        <Route path="/resume" component={About}/>
-                        <Route path="/contact" component={About}/>
+                        <Route default path="/" component={About}/>
                     </main>
                     </div>
                 </Router>
+                <Sticky>
+                    <Segment vertical attached='bottom'>
+                        <Container textAlign={'center'}>
+                            <Icon name={'copyright'}/>
+                            2017 Sarah Medeiros
+                        </Container>
+                    </Segment>
+                </Sticky>
             </div>
         )
     }
--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,11 +1,42 @@
 import React, {Component} from 'react';
-
+import {Segment, Container, Header, Icon} from 'semantic-ui-react'
 
 class About extends Component {
 
 
     render() {
-        return <div>
-            about</div>
+        return (
+            <div>
+                <Segment>
+                    <Container text>
+                        <Header textAlign={'center'} size={'large'}>
+                            <Header.Content content={'About Me'}/>
+                        </Header>
+                    </Container>
+                    <Container text textAlign={'center'} style={{padding: '30px'}}>
+                        I'm Sarah Medeiros a software developer at Liberty Mutual.
+                        I am currently in their entry level TechStart program.
+                        I graduated from Boston University in 2017 with a BA in Computer
+                        Science. I am interested in web development and security.
+                        In my spare time I love to bake, read, and knit. But I also enjoy
+                        learning new things. If you want to work together on a project just
+                        send me an email
+                        Feel free to reach out to me on LinkedIn or via email.
+                    </Container>
+                    <Container textAlign={'center'}>
+                            <a href={'https://linkedin.com/in/sarah-medeiros-0978b1112/'} target="_blank">
+                                <Icon color={'black'} name={'linkedin'}/>
+                            </a>
+                            <a href={'mailto:sarahpmedeiros@gmail.com'} target="_blank">
+                                <Icon color={'black'} name={'mail'}/>
+                            </a>
+                            <a href={'https://github.com/sarahpmedeiros'} target="_blank">
+                                <Icon color={'black'} name={'github'}/>
+                            </a>
+                    </Container>
+                </Segment>
+            </div>)
     }
-} export default About
+}
+
+export default About
